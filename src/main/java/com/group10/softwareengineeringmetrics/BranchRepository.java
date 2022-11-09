5def341a35eb7e97f2f9db91b5d962203685f267
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2022-11-09 15:15:02 +0000

    Revert "Set up initial database"
    This reverts commit 48ec61e3eec0e499c01edf470067108823857cff.

--- src/main/java/com/group10/softwareengineeringmetrics/BranchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group10.softwareengineeringmetrics;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BranchRepository extends CrudRepository<Branch, Integer> {
-    Branch findBranchById(Integer id);
-}
