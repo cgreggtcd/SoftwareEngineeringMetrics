88be16eda4f1188b85f424f93de01ba646dedc9a
Hannah Foley <foleyh1@tcd.ie>
2022-11-11 12:15:20 +0000

    removing for the minute the extra models

--- src/main/java/com/group10/softwareengineeringmetrics/repository/RepositoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group10.softwareengineeringmetrics.repository;
-
-import com.group10.softwareengineeringmetrics.models.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryRepository extends JpaRepository<Repository, Long> {
-}
--- src/main/java/com/group10/softwareengineeringmetrics/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group10.softwareengineeringmetrics.repository;
-
-import com.group10.softwareengineeringmetrics.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
