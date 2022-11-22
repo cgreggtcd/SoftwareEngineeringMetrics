6c5d4facf2e28a59f18011d2b36d1a35ed58ef6b
Hannah Foley <foleyh1@tcd.ie>
2022-11-22 20:19:01 +0000

    work breakdown backend

---
+++ src/main/java/com/group10/softwareengineeringmetrics/WorkBreakdown.java
@@ -0,0 +1,29 @@
+package com.group10.softwareengineeringmetrics;
+
+import com.group10.softwareengineeringmetrics.models.Commit;
+
+import java.util.List;
+
+public class WorkBreakdown {
+    DatabaseController dbcontroller = new DatabaseController();
+    List<Commit> commits = (List<Commit>) dbcontroller.getCommits();
+
+    String [][] getWorkBreakdownData () {
+        String [][] workBreakdownData = new String[commits.size()][4];
+
+        for(int i = 0; i < commits.size(); i++)
+        {
+            //Author name
+            workBreakdownData[i][0] = commits.get(i).getAuthorName();
+            //additions
+            workBreakdownData[i][1] = String.valueOf(commits.get(i).getAdditions());
+            //deletions
+            workBreakdownData[i][2] = String.valueOf(commits.get(i).getDeletions());
+            //refactors
+            workBreakdownData[i][1] = String.valueOf(commits.get(i).getChanges());
+        }
+
+        return workBreakdownData;
+    }
+
+}
