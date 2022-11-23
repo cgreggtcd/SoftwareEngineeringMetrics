95e0d260e803c903da53944ea87bd98681af1aee
conall47 <cotoole1@tcd.ie>
2022-11-23 13:08:54 +0000

    Edited controllers

--- src/main/java/com/group10/softwareengineeringmetrics/api/PullRequestControllerAPI.java
+++ src/main/java/com/group10/softwareengineeringmetrics/api/PullRequestControllerAPI.java
@@ -2,13 +2,14 @@
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.http.MediaType;
         import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RequestMethod;
         import org.springframework.web.bind.annotation.RestController;
         import org.springframework.web.client.RestTemplate;
 
 
-        @RestController
+        @Component
 
         public class PullRequestControllerAPI {
 
--- src/main/java/com/group10/softwareengineeringmetrics/api/UserControllerAPI.java
+++ src/main/java/com/group10/softwareengineeringmetrics/api/UserControllerAPI.java
@@ -4,13 +4,13 @@ package com.group10.softwareengineeringmetrics.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Component
 public class UserControllerAPI {
     @Autowired
     private RestTemplate restTemplate;
