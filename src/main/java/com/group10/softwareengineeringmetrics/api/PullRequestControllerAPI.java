54bb87a9a5bf271fd7301446b4d3ac08f82e1efa
cgreggtcd <98959891+cgreggtcd@users.noreply.github.com>
2022-11-24 15:14:30 +0000

    fix pull request for merge

--- src/main/java/com/group10/softwareengineeringmetrics/api/PullRequestControllerAPI.java
+++ src/main/java/com/group10/softwareengineeringmetrics/api/PullRequestControllerAPI.java
@@ -3,24 +3,22 @@ package com.group10.softwareengineeringmetrics.api;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
 @Component
 public class PullRequestControllerAPI {
 
-  @Autowired
-  private RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    private RestTemplate restTemplate = new RestTemplate();
 
-  @Value("${github.access.token}")
-  private String githubAccessToken;
+    @Value("${github.access.token}")
+    private String githubAccessToken;
+
+    private final String git_api_url = "https://api.github.com";
 
     /*
             Method gets the pulls requests on a given repository and returns a JSON String entity
@@ -29,29 +27,18 @@ public class PullRequestControllerAPI {
                     String owner (name of the repo owner)
     */
     //@RequestMapping(value = "/main", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object[]> getPulls(String owner, String name) {
-        ResponseEntity<Object[]> response = restTemplate.getForEntity(git_api_url + "/repos/" + owner + "/" + name
-                + "/pulls", Object[].class);
+    public ResponseEntity<Object[]> getPulls(String username, String repoName) {
+        String url = git_api_url + "/repos/" + username + "/" + repoName + "/pulls?state=all";
+        HttpEntity<Void> entity = getAuthorizationHeaderEntity();
+        ResponseEntity<Object[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Object[].class);
         return response;
+    }
 
-                Params: String name (name of chosen repo)
-                        String owner (name of the repo owner)
-        */
-        public ResponseEntity<Object[]> getPulls (String username, String repoName)  {
-                String url = git_api_url + "/repos/" + username + "/" + repoName + "/pulls?state=all";
-                HttpEntity<Void> entity = getAuthorizationHeaderEntity();
-                ResponseEntity<Object[]> response = restTemplate.exchange(url, HttpMethod.GET, entity, Object[].class);
-                return response;
-        }
-
-        private HttpEntity<Void> getAuthorizationHeaderEntity(){
-                HttpHeaders headers = new HttpHeaders();
-                headers.set("Authorization",githubAccessToken);
-                return new HttpEntity<>(headers);
-        }
-
-
-
+    private HttpEntity<Void> getAuthorizationHeaderEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", githubAccessToken);
+        return new HttpEntity<>(headers);
+    }
 
 
 //        @RequestMapping(value = "/main", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -61,5 +48,5 @@ public class PullRequestControllerAPI {
 //                return response;
 //
 //        }
-        }
+}
 
