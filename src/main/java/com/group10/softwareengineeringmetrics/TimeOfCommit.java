ae202fdfc09c1df2f9b0b77733557ee1914d8e8e
Hannah Foley <foleyh1@tcd.ie>
2022-11-24 16:01:43 +0000

    time of commits back end

---
+++ src/main/java/com/group10/softwareengineeringmetrics/TimeOfCommit.java
@@ -0,0 +1,21 @@
+package com.group10.softwareengineeringmetrics;
+
+import com.group10.softwareengineeringmetrics.models.Commit;
+
+import java.util.List;
+
+public class TimeOfCommit {
+    DatabaseController dbcontroller = new DatabaseController();
+    List<Commit> commits = (List<Commit>) dbcontroller.getCommits();
+
+    String [] getTimeOfCommits () {
+        String[] timeOfCommits = new String[commits.size()];
+
+        for (int i = 0; i < commits.size(); i++) {
+            timeOfCommits[i] = commits.get(i).getTime();
+
+        }
+
+        return timeOfCommits;
+    }
+}
